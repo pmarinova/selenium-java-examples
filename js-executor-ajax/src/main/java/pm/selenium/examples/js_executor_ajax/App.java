0177df1ac88873b3042308afab850ff57fb38427
Plamena Marinova <plamena.marinova@gmail.com>
2024-02-11 16:33:08 +0200

    Initial project structure

---
+++ js-executor-ajax/src/main/java/pm/selenium/examples/js_executor_ajax/App.java
@@ -0,0 +1,13 @@
+package pm.selenium.examples.js_executor_ajax;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
