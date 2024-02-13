c180ab7bf2d4b2417a7376088b888cfe9c169055
Plamena Marinova <plamena.marinova@gmail.com>
2024-02-13 15:48:51 +0200

    Add browsermob proxy example

---
+++ browsermob-proxy/README.md
@@ -0,0 +1,1 @@
+Minimal example with [BrowserMob Proxy](https://github.com/lightbody/browsermob-proxy). To use HTTPS you need to install the [ca-certificate-rsa.cer](https://github.com/lightbody/browsermob-proxy/blob/master/browsermob-core/src/main/resources/sslSupport/ca-certificate-rsa.cer) certificate file in your Chrome browser.
--- browsermob-proxy/src/main/java/pm/selenium/examples/browsermob_proxy/App.java
+++ browsermob-proxy/src/main/java/pm/selenium/examples/browsermob_proxy/App.java
@@ -1,13 +1,64 @@
 package pm.selenium.examples.browsermob_proxy;
 
-/**
- * Hello world!
- *
- */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
-    }
+import java.text.SimpleDateFormat;
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+
+import net.lightbody.bmp.BrowserMobProxyServer;
+import net.lightbody.bmp.client.ClientUtil;
+import net.lightbody.bmp.proxy.CaptureType;
+
+public class App {
+
+	static {
+		System.setProperty("org.slf4j.simpleLogger.defaultLogLevel", "error");
+	}
+
+	public static void main(String[] args) {
+
+		BrowserMobProxyServer proxy = null;
+		ChromeDriver chrome = null;
+
+		try {
+			proxy = new BrowserMobProxyServer();
+			proxy.start();
+			System.out.println("Proxy started at port " + proxy.getPort());
+
+			var options = new ChromeOptions();
+			options.setProxy(ClientUtil.createSeleniumProxy(proxy));
+			chrome = new ChromeDriver(options);
+			System.out.println("Chrome started");
+
+			proxy.enableHarCaptureTypes(CaptureType.REQUEST_CONTENT, CaptureType.RESPONSE_CONTENT);
+			proxy.newHar("google.com");
+
+			chrome.navigate().to("https://www.google.com/");
+
+			System.out.println("Waiting for network traffic to stop...");
+			proxy.waitForQuiescence(1, 5, TimeUnit.SECONDS);
+
+			proxy.getHar().getLog().getEntries().forEach((entry) -> {
+				System.out.println(String.format("[%s] \"%s %s %s\" %d %d",
+						new SimpleDateFormat("dd/MMM/yyyy:HH:mm:ss Z").format(entry.getStartedDateTime()),
+						entry.getRequest().getMethod(), 
+						entry.getRequest().getUrl(),
+						entry.getRequest().getHttpVersion(), 
+						entry.getResponse().getStatus(),
+						entry.getResponse().getBodySize()));
+			});
+
+		} finally {
+			if (chrome != null) {
+				chrome.quit();
+				System.out.println("Chrome stopped");
+			}
+			if (proxy != null) {
+				proxy.stop();
+				System.out.println("Proxy stopped");
+			}
+		}
+	}
+
 }
