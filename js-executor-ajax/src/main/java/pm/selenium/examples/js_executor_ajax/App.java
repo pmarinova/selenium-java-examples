1a56ce656108139689408917d95181de99508398
Plamena Marinova <plamena.marinova@gmail.com>
2024-02-14 15:24:21 +0200

    Add javascript executor example

--- js-executor-ajax/src/main/java/pm/selenium/examples/js_executor_ajax/App.java
+++ js-executor-ajax/src/main/java/pm/selenium/examples/js_executor_ajax/App.java
@@ -1,13 +1,69 @@
 package pm.selenium.examples.js_executor_ajax;
 
-/**
- * Hello world!
- *
- */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
-    }
+import static org.openqa.selenium.remote.http.HttpMethod.GET;
+import static org.openqa.selenium.remote.http.HttpMethod.POST;
+
+import java.util.Map;
+
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.remote.http.HttpMethod;
+
+public class App {
+	
+	private static final String BASE_URL = "https://httpbin.org";
+	
+	private final WebDriver driver;
+	private final JavascriptExecutor jse;
+	
+	public static void main(String[] args) {
+		new App(new ChromeDriver()).run();
+	}
+	
+	public App(WebDriver driver) {
+		this.driver = driver;
+		this.jse = (JavascriptExecutor)driver;
+	}
+	
+	public void run() {
+		try {
+			driver.navigate().to(BASE_URL);
+			
+			Map<?,?> response = null; 
+			
+			// GET request with fetch()
+			response = fetch(GET, BASE_URL + "/get", Map.of("param1", "value1", "param2", "value2"));
+			System.out.println(response);
+			
+			// POST request with fetch()
+			response = fetch(POST, BASE_URL + "/post", Map.of("param1", "value1", "param2", "value2"));
+			System.out.println(response);
+			
+		} finally {
+			this.driver.quit();
+		}
+	}
+	
+	private Map<?,?> fetch(HttpMethod method, String url, Map<String, String> params) {
+		var script = """
+			const method = arguments[0];
+			const url = arguments[1];
+			const params = arguments[2];
+			
+			const queryParams = (method === 'GET') ? 
+			  '?' + Object.keys(params).map(k => `${encodeURIComponent(k)}=${encodeURIComponent(params[k])}`).join('&') : '';
+			
+			const options = {
+			  method,
+			  body: (method === 'POST') ? JSON.stringify(params) : undefined
+			};
+			
+			const response = await fetch(url + queryParams, options);
+			const json = await response.json();
+			return json;
+		""";
+		
+		return (Map<?,?>)jse.executeScript(script, method.name(), url, params);
+	}
 }
